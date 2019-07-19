74edb738cb0bb3dd2626d3870da5ba0c177d5db6
Patrick <patrick3948@gmail.com>
2019-07-20 02:22:46 +0800

    Udpate config for fix building issue

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -349,7 +349,7 @@ module.exports = function(webpackEnv) {
                   ],
                   isEnvDevelopment ? 'react-hot-loader/babel' : undefined,
                   'react-loadable/babel',
-                ],
+                ].filter(Boolean),
                 // This is a feature of `babel-loader` for webpack (not Babel itself).
                 // It enables caching results in ./node_modules/.cache/babel-loader/
                 // directory for faster rebuilds.
